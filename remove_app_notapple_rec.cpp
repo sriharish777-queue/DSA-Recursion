b860d9de8ff72b0f91230992c198ff299b19baa0
sriharish777-queue <sriharish2006@gmail.com>
2025-09-10 19:38:28 +0530

    Add files via upload

---
+++ remove_app_notapple_rec.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+string remove_string(string str,string target,string new_str,int index){
+    if(index>=str.length()){
+        return new_str;
+    }
+    if(str.substr(index,target.length())==target&&str.substr(index,target.length()+2)!="apple"){
+        return remove_string(str,target,new_str,index+target.length());
+    }
+    else{
+        new_str=new_str+str[index];
+        return remove_string(str,target,new_str,index+1);
+    }
+}
+int main(){
+    string s;
+    cin>>s;
+    string target="app";
+    string s1="";
+    cout<<remove_string(s,target,s1,0);
+}
